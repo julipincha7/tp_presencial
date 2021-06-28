9fd0fe4fa2dac815d56d49ad96bb214f99f9826c
julian <leon.pincha@hotmail.com>
2021-06-28 15:28:11 -0300

    Agregando estilos 1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const app= express();
+
+const path= require("path");
+const port=3011;
+
+/* midleware*/
+app.use(express.static("public"));
+
+/*rutas*/
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}\nAbrir en el navegador http://localhost:${port}
+`))
